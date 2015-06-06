710af4ef167787e148a5d7594520dfb1af9cf3d9
PSzalapski <szalap@hotmail.com>
2015-06-06 18:53:55 -0500

    enh

---
+++ Limiter.Tests.Unit/AzureLimitStoreTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Limiter.Tests.Unit {
+    [TestClass]
+    public class AzureLimitStoreTests {
+        private const string appKey = "unitTestsAppKey";
+
+        [TestMethod]
+        public void Limit_DefaultBucketFor2PerMinuteCallOnce_DoesNotLimit() {
+            var store = new AzureLimitStore(2);
+            bool result = store.Limit(appKey, "1.2.3.1");
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void Limit_DefaultBucketFor2PerMinuteCallThrice_Limits() {
+            var limiter = new AzureLimitStore(2);
+            string testLimitKey = "1.2.3.2";
+            limiter.Limit(appKey, testLimitKey);
+            limiter.Limit(appKey, testLimitKey);
+
+            bool result = limiter.Limit(appKey, testLimitKey);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Limit_DefaultBucketFor2PerHalfMinuteCallOnce_DoesNotLimit() {
+            var store = new AzureLimitStore(2, TimeSpan.FromSeconds(30));
+
+            bool result = store.Limit(appKey, "1.2.3.3");
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void Limit_DefaultBucketFor2PerHalfMinuteCallThrice_Limits() {
+            var limiter = new AzureLimitStore(2, TimeSpan.FromSeconds(30));
+            string testLimitKey = "1.2.3.4";
+            limiter.Limit(appKey, testLimitKey);
+            limiter.Limit(appKey, testLimitKey);
+            bool result = limiter.Limit(appKey, testLimitKey);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Limiter/AzureLimitStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Limiter { 
+    public class AzureLimitStore : ILimitStore {
+        public AzureLimitStore(int hitLimitPerMinute) {
+            _hitLimit = hitLimitPerMinute;
+            _limitInterval = TimeSpan.FromMinutes(1);
+        }
+
+        public AzureLimitStore(int hitLimit, TimeSpan interval) {
+            _hitLimit = hitLimit;
+            _limitInterval = interval;
+
+        }
+        private int _hitLimit;
+        private TimeSpan _limitInterval = TimeSpan.FromMinutes(1);
+
+        public bool Limit(string _appKey, string limitKey) {
+            throw new NotImplementedException();
+        }
+
+        public void SetLimit(int hitsPerMinute) {
+            throw new NotImplementedException();
+        }
+    }
+}
