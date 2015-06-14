d8e82163d0a8eee6eba2e35419ff194f98c8e683
PSzalapski <szalap@hotmail.com>
2015-06-14 11:05:17 -0500

    code cleanup

--- Limiter/Limiter.cs
+++ Limiter/Limiter.cs
@@ -1,28 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Limiter;
+﻿using Limiter;
 
-namespace SzLimiter
-{
-    public class Limiter
-    {
-
-        // bastard constructor for public API
-        public Limiter(string appKey) {
-            Initialize(appKey);
-        }
-
-        public Limiter(string appKey, ILimitStore _store) {
-            Initialize(appKey);
+namespace SzLimiter {
+    public class Limiter {
+        public Limiter(string appKey, ILimitStore store) {
+            _appKey = appKey;
+            _store = store;
         }
         private ILimitStore _store { get; set; }
-        
-        private void Initialize(string appKey){ 
-            _appKey = appKey;
-        }
         private string _appKey {get; set;}
 
         public bool Limit(string limitKey) {
