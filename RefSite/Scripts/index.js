436614074f208077bfb0f2e69de387af7da1635e
PSzalapski <szalap@hotmail.com>
2015-06-26 23:39:32 -0500

    basics of web site

--- RefSite/Scripts/index.js
+++ RefSite/Scripts/index.js
@@ -1,19 +1,36 @@
-﻿function foo(data){
-    $('#fooMessage').text("invoked foo " + Date.now());
-
+﻿function foo() {
+    // do stuff
+    setFooMessage("Foo invoked!");
 }
+
 
 $('#fooButton').click(function (eventObject) {
     $.ajax({
         type: 'POST',
         url: 'http://limtr.azurewebsites.net/api/limit?appKey=free&limitKey=foo',
-        success: foo,
-        error: function () {
-            $('#fooMessage').text("foo error");
-
+        success: function (isAllowed) {
+            if (isAllowed) foo();
+            else setFooMessage("Sorry, you have made too many attempts. Please try again later.");
+        },
+        error: function () { 
+            setFooMessage("Limtr Error");
         }
     });
 });
 
+function setFooMessage(value) {
+    $('#fooMessage').text(value + " " + Date.now());
+}
 
-$('#barMessage').text("further implementation");
+
+
+$('#myButton').click(function (eventObject) {
+    $.ajax({
+        type: 'POST',
+        url: 'http://limtr.azurewebsites.net/api/limit?appKey=free&limitKey=myOperation1',
+        success: function(isAllowed) {
+            if (isAllowed) { /* invoke the full functionality here */ }
+            else alert("Sorry, you have made too many attempts. Please try again later.");
+        }
+    });
+});
