8e84aa6f0419a72b072751e00a4433137aa60ac5
PSzalapski <szalap@hotmail.com>
2015-06-26 23:02:32 -0500

    tweaks

--- RefSite/Scripts/index.js
+++ RefSite/Scripts/index.js
@@ -1,8 +1,18 @@
-﻿$('#fooMessage').click(function (eventObject) {
+﻿function foo(data){
+    $('#fooMessage').text("invoked foo " + Date.now());
+
+}
+
+$('#fooButton').click(function (eventObject) {
     $.ajax({
         type: 'POST',
-        url: ''
-    })
+        url: 'http://limtr.azurewebsites.net/api/limit?appKey=free&limitKey=foo',
+        success: foo,
+        error: function () {
+            $('#fooMessage').text("foo error");
+
+        }
+    });
 });
 
 
