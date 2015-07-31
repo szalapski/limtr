8caa5647ba660615f39808533620ffe83c4c0350
PSzalapski <szalap@hotmail.com>
2015-07-31 13:36:29 -0500

    enh

--- Limtr/Limiter.cs
+++ Limtr/Limiter.cs
@@ -16,11 +16,26 @@ namespace Limtr {
         /// </summary>
         /// <returns>True if the operation should be allowed, and false if the operation should be rejected or throttled.</returns>
         public bool Allows(string operationKey) {
+            return AllowedPrivate(operationKey, doPost: true);
+        }
+
+        /// <summary>
+        /// Peeks at whether the operation represented by the limit key is allowed. Does not record a hit.
+        /// </summary>
+        /// <returns>True if the operation should be allowed, and false if the operation should be rejected or throttled.</returns>
+        public bool IsAllowed(string operationKey) {
+            return AllowedPrivate(operationKey, doPost: false);
+        }
+
+        private bool AllowedPrivate(string operationKey, bool doPost = false) {
             using (var client = new HttpClient()) {
                 client.BaseAddress = apiUri;
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = client.PostAsync($"Limit?appKey={_appKey}&bucketName=default&operationKey={operationKey}", new ByteArrayContent(new byte[0])).Result;
+                string limitQueryString = $"Limit?appKey={_appKey}&bucketName=default&operationKey={operationKey}";
+                HttpResponseMessage response = doPost
+                    ? client.PostAsync(limitQueryString, new ByteArrayContent(new byte[0])).Result
+                    : client.GetAsync(limitQueryString).Result;
                 if (response.IsSuccessStatusCode) {
                     return response.Content.ReadAsStringAsync().Result == "true";
                 }
@@ -30,20 +45,13 @@ namespace Limtr {
             }
         }
 
-        /// <summary>
-        /// Peeks at whether the operation represented by the limit key is allowed. Does not record a hit.
-        /// </summary>
-        /// <returns>True if the operation should be allowed, and false if the operation should be rejected or throttled.</returns>
-        public bool IsAllowed(string operationKey) {
-            throw new NotImplementedException();
-        }
 
         /// <summary>
         /// If the operation represented by the limit key is allowed, records a hit; throws exception if the operation should be rejected or throttled.
         /// </summary>
         /// <exception cref="LimitReachedException">Thrown if the operation should be rejected or throttled</exception>
         public void Hit(string operationKey) {
-            throw new NotImplementedException();
+            if (!AllowedPrivate(operationKey, doPost: true)) throw new InvalidOperationException(); // TODO: better exception type
         }
     }
 }
