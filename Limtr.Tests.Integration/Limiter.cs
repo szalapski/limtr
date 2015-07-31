e8d598483741043d2b3ef4a235f42afa694401e7
PSzalapski <szalap@hotmail.com>
2015-07-31 11:42:16 -0500

    revert to framework 4.5.2; waiting for Azure to support.  Start implementing Limiter .NET client.

---
+++ Limtr.Tests.Integration/Limiter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Limtr;
+
+namespace Limtr.Tests.Integration
+{
+    [TestClass]
+    public class LimiterTests
+    {
+        [TestMethod]
+        public void Allows_1()
+        {
+            var limiter = new Limiter("free");
+            for (int i = 0; i < 20; i++)
+            {
+                if (limiter.Allows("myOp1"))
+                {
+                    /* invoke the full functionality here */
+                    Console.WriteLine("Executed.");
+                }
+                else
+                {
+                    Console.Error.WriteLine("Sorry, you have made too many attempts. Please try again later.");
+                }
+            }
+        }
+    }
+}
--- Limtr/Limiter.cs
+++ Limtr/Limiter.cs
@@ -1,31 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Limtr
 {
     public class Limiter
     {
-         public Limiter(string appKey) {
+        public Limiter(string appKey) {
             _appKey = appKey;
         }
         private string _appKey {get; set;}
+
+        public Uri apiUri { get; set; } = new Uri("http://limtr.azurewebsites.net/api/");
 
         /// <summary>
         /// If the operation represented by the limit key is allowed, records a hit.
         /// </summary>
         /// <returns>True if the operation should be allowed, and false if the operation should be rejected or throttled.</returns>
-        public bool Allows(string limitKey) {
-            throw new NotImplementedException();
+        public bool Allows(string operationKey) {
+            using (var client= new HttpClient())
+            {
+                client.BaseAddress = apiUri;
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync($"Limit?appKey={_appKey}&bucketName=default&operationKey={operationKey}").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                  
+                }
+                else
+                {
+                    //Something has gone wrong, handle it here
+                }
+
+            }
+            return false;
         }
 
         /// <summary>
         /// Peeks at whether the operation represented by the limit key is allowed. Does not record a hit.
         /// </summary>
         /// <returns>True if the operation should be allowed, and false if the operation should be rejected or throttled.</returns>
-        public bool IsAllowed(string limitKey) {
+        public bool IsAllowed(string operationKey) {
             throw new NotImplementedException();
         }
 
@@ -33,7 +49,7 @@ namespace Limtr
         /// If the operation represented by the limit key is allowed, records a hit; throws exception if the operation should be rejected or throttled.
         /// </summary>
         /// <exception cref="LimitReachedException">Thrown if the operation should be rejected or throttled</exception>
-        public void Hit(string limitKey) {
+        public void Hit(string operationKey) {
             throw new NotImplementedException();
         }
     }
